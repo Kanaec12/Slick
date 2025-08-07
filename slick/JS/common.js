f8795964fe09d6ef72e4d4f9d8e008e8fa407b0e
Kanaec12 <kanae.carta@gmail.com>
2025-08-07 19:20:16 +0900

    Delete slick/JS directory

--- slick/JS/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.slick01').slick({
-        autoplay:true,
-        dots:true,
-        arrows:true,
-        prevArrow:'<i class="fas fa-arrow-left arrow-icon-left"></i>',
-        nextArrow:'<i class="fas fa-arrow-right arrow-icon-right"></i>'
-    });
-});
